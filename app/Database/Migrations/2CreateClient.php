4a4f7eaa8d67ca95748dcbf67a1faea1c16f9556
Faneva <faneva.raffan@gmail.com>
2026-06-17 09:54:16 +0300

    Database migration

---
+++ app/Database/Migrations/2CreateClient.php
@@ -0,0 +1,35 @@
+<?php
+ 
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateClient extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nom' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'contact' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('client');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('client');
+    }
+}
