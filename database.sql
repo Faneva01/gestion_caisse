30cabdd92ebeb8a5115840191cbef301b34f394e
Faneva <faneva.raffan@gmail.com>
2026-06-17 09:08:58 +0300

    First commit and repo initializing

---
+++ database.sql
@@ -0,0 +1,20 @@
+-- Table 
+produit:
+    - id
+    - designation
+    - prix_unitaire
+
+caisse:
+    - id
+
+achat:
+    - id
+    - nom_client
+
+achat_produit:
+    - id
+    - id_achat
+    - id_produit
+    - id_caisse
+    - quantite
+    - date_achat
